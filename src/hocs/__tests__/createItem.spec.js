435c5ae40832e336fec6890554d63415e1205305
Cristian Muscalu <muscaiu@yahoo.com>
2019-02-04 18:01:18 +0200

    added 'connect' tests

--- src/hocs/__tests__/createItem.spec.js
+++ src/hocs/__tests__/createItem.spec.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import createItem from '../createItem';
+
+jest.resetModules();
+jest.mock('react-redux');
+
 const WrappedComponent = () => (<li />);
+let createItem;
 
-describe('createItem', () => {
-  it('should create an Item component', () => {
-    let Item = createItem(<WrappedComponent/>);    
-    expect(Item).toBeDefined();
-  })
-})
-
-describe('connect', () =>{
-  it('should call mapStateToProps()',() =>{
-    // jest.resetModules();
-    // jest.mock('react-redux');
+describe('connect', () => {
+  it('should call mapStateToProps()', () => {
+    jest.resetModules();
+    jest.mock('react-redux');
     const { connect } = require('react-redux');
     connect.mockReturnValue(jest.fn);
+    createItem = require('../createItem').default;
     createItem(WrappedComponent);
     expect(connect).toHaveBeenCalled();
     expect(connect.mock.calls[0][0]).toHaveProperty('name', 'mapStateToProps');
   })
+
+  describe('mapStateToProps', () => {
+    let mapStateToProps;
+
+    beforeEach(() => {
+      const { connect } = require('react-redux');
+      connect.mockReturnValue(jest.fn);
+      createItem = require('../createItem').default;
+      createItem(WrappedComponent);
+      mapStateToProps = connect.mock.calls[0][0];
+    });
+  });
 })
